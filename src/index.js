53f02d9f7a4f155ff33fb43ca26adb448714f6ea
unknown <yejz0715@gmail.com>
2023-01-06 17:46:50 +0900

    redux-action 개념공부

--- src/index.js
+++ src/index.js
@@ -5,16 +5,29 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-//state: app에서 바뀌는 data
-//reducer
-const countModifier = (state = 0) => {
-  //state 초기화
-  return state; //app에 있는 data
-};
-const countStore = createStore(countModifier); //store:data넣을 수 있는 장소-> 생성
-
-console.log(countStore.getState()); //0
 //스토어가 갖고있는 내장함수:dispatch,subscribe, getState,replaceReducer
+//store:data넣을 수 있는 장소
+//state: app에서 바뀌는 data
+//reducer는 두가지 파라미터를 갖는다(state(현재상태), action(객체,type필수))
+//reducer의 return 값은 app의 data
+//action을 리듀서로 보내는 방법은 countStore.dispatch();
+const countModifier = (state = 0, action) => {
+  if (action.type === "ADD") {
+    return state + 1;
+  } else if (action.type === "MINUS") {
+    return state - 1;
+  } else {
+    return state;
+  }
+};
+const countStore = createStore(countModifier); //스토어 생성
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
 
 /* vanilla javascript
 아래의 내용의 목적은 count를 수정하기 위해(+하거나, -하거나)
